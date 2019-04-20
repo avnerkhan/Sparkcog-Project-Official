d42e8d6d7981a40ea055d66c18d37a5333959c0a
Kiran Raja <kiran.raja22@gmail.com>
2019-04-20 13:20:49 -0500

    cleaned up code and ran test with 1 hr of training

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 
-writer_file = open("Crime_Reports_Fixed.csv", "w", newline="", encoding="utf-8")
+writer_file = open("Crime_Reports_Fixed_Labels.csv", "w", newline="", encoding="utf-8")
 
 with open("Crime_Reports.csv", encoding="utf-8") as crime_large_csv:
     csv_writer = csv.writer(writer_file)
@@ -10,13 +10,9 @@ with open("Crime_Reports.csv", encoding="utf-8") as crime_large_csv:
     for row in csv_reader:
         if (line_count > 0):
             if (row[18] != 'C' and row[18] != ''):
-                # print('Adjusting status: ', row[18])
                 row[18] = 'N'
         csv_writer.writerow(row)
-        # print(row, line_count)
         line_count += 1
-        # if line_count == 1000:
-        #     break
 
-dataset = pd.read_csv("Crime_Reports_Fixed.csv")
+dataset = pd.read_csv("Crime_Reports_Fixed_Labels.csv")
 print(len(dataset))
